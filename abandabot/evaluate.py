f72ac685c18d8f33c4b0b17480e1f15adb5df73e
Hao He <haohe@andrew.cmu.edu>
2025-02-05 10:16:28 -0500

    Refactor to prepare for AI agent implementation

--- abandabot/evaluate.py
+++ abandabot/evaluate.py
@@ -5,6 +5,7 @@ import shutil
 import logging
 import argparse
 import subprocess
+import pandas as pd
 
 
 from abandabot import REPO_PATH, CODEQL_DB_PATH, REPORT_PATH
@@ -17,10 +18,10 @@ def remove_readonly(func, path, _):
     func(path)
 
 
-def clone_repo(repo_name: str, overwrite: bool = False) -> None:
+def clone_repo(repo: str, overwrite: bool = False) -> None:
     os.makedirs(REPO_PATH, exist_ok=True)
 
-    repo_path = os.path.join(REPO_PATH, repo_name.replace("/", "_"))
+    repo_path = os.path.join(REPO_PATH, repo.replace("/", "_"))
 
     if os.path.exists(repo_path):
         if overwrite:
@@ -30,12 +31,46 @@ def clone_repo(repo_name: str, overwrite: bool = False) -> None:
             logging.info("Repository already cloned to %s, skipping", repo_path)
             return
 
-    logging.info(f"Cloning repository {repo_name}")
+    logging.info(f"Cloning repository {repo}")
     subprocess.run(
-        ["git", "clone", f"https://github.com/{repo_name}.git", repo_path],
+        ["git", "clone", f"https://github.com/{repo}.git", repo_path],
         check=True,
         stdout=subprocess.PIPE,
     )
+
+
+def find_dep_usage_codeql(repo: str, overwrite: bool) -> pd.DataFrame:
+    clone_repo(repo, overwrite=overwrite)
+
+    repo_path = os.path.join(REPO_PATH, repo.replace("/", "_"))
+    database_path = os.path.join(CODEQL_DB_PATH, repo.replace("/", "_"))
+    report_path = os.path.join(REPORT_PATH, f"{repo.replace('/', '_')}")
+    os.makedirs(report_path, exist_ok=True)
+
+    if not os.path.exists(os.path.join(repo_path, "package.json")):
+        logging.info("Skipping CodeQL database creation, no package.json found")
+        logging.info("Currently only JS/TS projects using npm are supported")
+        return
+
+    create_database(
+        repo_path,
+        database_path,
+        language="javascript",
+        overwrite=overwrite,
+    )
+
+    execute_query(
+        query_path="queries/find_dep_usage.ql",
+        database_path=database_path,
+        output_path=os.path.join(report_path, "dep-usage.bqrs"),
+        decode_path=os.path.join(report_path, "dep-usage.csv"),
+    )
+
+    return pd.read_csv(os.path.join(report_path, "dep-usage.csv"))
+
+
+def generate_report(repo: str, dep: str, dep_usage: pd.DataFrame) -> None:
+    raise NotImplementedError("Report generation is not implemented yet")
 
 
 def main():
@@ -58,39 +93,32 @@ def main():
         help="GitHub repository in the format 'owner/repo'",
     )
     parser.add_argument(
+        "--dep",
+        type=str,
+        required=True,
+        help="The abandoned dependency to evaluate (e.g. 'lodash')",
+    )
+    parser.add_argument(
         "--overwrite",
         action="store_true",
         help="Overwrite the GitHub repository and CodeQL DB if it already exists",
     )
     args = parser.parse_args()
 
-    logging.info(f"GitHub repository: {args.github}")
+    logging.info("GitHub repository: %s", args.github)
 
-    clone_repo(args.github, overwrite=args.overwrite)
-
-    repo_path = os.path.join(REPO_PATH, args.github.replace("/", "_"))
-    database_path = os.path.join(CODEQL_DB_PATH, args.github.replace("/", "_"))
-    report_path = os.path.join(REPORT_PATH, f"{args.github.replace('/', '_')}")
-    os.makedirs(report_path, exist_ok=True)
-
-    if not os.path.exists(os.path.join(repo_path, "package.json")):
-        logging.info("Skipping CodeQL database creation, no package.json found")
-        logging.info("Currently only JS/TS projects using npm are supported")
+    dep_usage = find_dep_usage_codeql(args.github, args.overwrite)
+    if args.dep not in set(dep_usage.name):
+        logging.info(
+            "Dependency %s not found in %s, found deps are: ",
+            args.dep,
+            args.github,
+            set(dep_usage.name),
+        )
         return
 
-    create_database(
-        repo_path,
-        database_path,
-        language="javascript",
-        overwrite=args.overwrite,
-    )
-
-    execute_query(
-        query_path="queries/find_dep_usage.ql",
-        database_path=database_path,
-        output_path=os.path.join(report_path, "dep-usage.bqrs"),
-        decode_path=os.path.join(report_path, "dep-usage.csv"),
-    )
+    generate_report(args.github, args.dep, dep_usage)
+    logging.info("Report generated successfully")
 
 
 if __name__ == "__main__":
